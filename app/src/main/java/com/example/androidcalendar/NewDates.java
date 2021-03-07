144f9e32fb42af7e32255b8ff33a47ad287e7522
Michaellu <mchblch@protonmail.com>
2021-03-07 10:26:15 +0100

    Moved List deploy

--- app/src/main/java/com/example/androidcalendar/NewDates.java
+++ app/src/main/java/com/example/androidcalendar/NewDates.java
@@ -3,11 +3,18 @@ package com.example.androidcalendar;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,7 +23,8 @@ import android.view.ViewGroup;
  */
 public class NewDates extends Fragment {
 
-    private DateList m_list;
+    private RecyclerView m_rv;
+    private DateListAdapter m_da;
 
     public NewDates() {
         // Required empty public constructor
@@ -41,16 +49,46 @@ public class NewDates extends Fragment {
         View v = inflater.inflate(R.layout.fragment_new_dates, container, false);
 
         // Setup list
-        m_list = new DateList(v.findViewById(R.id.new_list));
+        m_rv = v.findViewById(R.id.new_list);
 
         // Add click listener
-        v.findViewById(R.id.newdate).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in = new Intent(getActivity(), DateCreate.class);
-                startActivityForResult(in, 0); // Start date creation
-            }
+        v.findViewById(R.id.newdate).setOnClickListener(v1 -> {
+            Intent in = new Intent(getActivity(), DateCreate.class);
+            startActivityForResult(in, 0); // Start date creation
         });
+
+        // Add vertical lines
+        m_rv.addItemDecoration(new DividerItemDecoration(m_rv.getContext(), DividerItemDecoration.VERTICAL));
+
+        // Create list adapter
+        m_da = new DateListAdapter();
+
+        // Test list
+        Entry e1 = new Entry();
+        e1.text = "Test1";
+        e1.date = Calendar.getInstance();
+
+        Entry e2 = new Entry();
+        e2.text = "Test2";
+        e2.date = Calendar.getInstance();
+
+        m_da.add(new Entry[]{e1, e2});
+
+        // Add swipe delete
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                m_da.remove(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(m_rv);
+
+        m_rv.setAdapter(m_da);
+        m_rv.setLayoutManager(new LinearLayoutManager(m_rv.getContext())); // Linear list
 
         return v;
     }
